8bd65dc0b598220aa5a9eee4e06f195915452905
Lutyvr02 <lutyvr02@gmail.com>
2023-08-28 22:10:44 -0400

    Change the swagger description commit

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,9 @@ async function bootstrap() {
 
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
+    .setTitle('GymyApp API')
+    .setDescription('API enfocada en manejar una base de datos la cual almacena cosas importantes')
+    .setVersion('2.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document, {

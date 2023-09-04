ea93e124cbf8b6a7b043ddfdaf228c2744f8c0eb
Lutyvr02 <lutyvr02@gmail.com>
2023-09-04 12:44:03 -0400

    Remove CI temporarly commit

--- src/clients/clients.entity.ts
+++ src/clients/clients.entity.ts
@@ -1,7 +1,7 @@
 import {Entity, Column, PrimaryGeneratedColumn} from 'typeorm'
 
 
-@Entity({name: 'clientes registrados' })
+@Entity({name: 'clientes registrados ' })
 export class Clients{
 
     @PrimaryGeneratedColumn()
--- src/employee/employee.entity.ts
+++ src/employee/employee.entity.ts
@@ -1,11 +1,7 @@
 import {Entity, Column, PrimaryGeneratedColumn} from 'typeorm'
 
 
-<<<<<<< HEAD
 @Entity({name: 'empleados3' })
-=======
-@Entity({name: 'empleados2' })
->>>>>>> 39d834257767242f104ef9736d35a6566c69904e
 export class Employee{
 
     @PrimaryGeneratedColumn()
